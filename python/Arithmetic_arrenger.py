c8b6ae56a6c824ca33809550af59d72c3dcf39b3
Jimmie <jimmieriss@gmail.com>
2021-03-08 21:39:17 +0100

    worked on computing the problem

--- python/Arithmetic_arrenger.py
+++ python/Arithmetic_arrenger.py
@@ -1,15 +1,12 @@
 def arithmetic_arranger(problems, optional=False):
-    
+
+    problem_dict = dict()
+  
     for problem in problems: 
         ps = problem.split()
-        if ps[1] == "+":
-            addition = int(ps[0]) + int(ps[2])
-        if ps[1] == "-":
-            addition = int(ps[0]) - int(ps[2])
+        prolem_dict[ps] = (int(ps[0]), (ps[1], int(ps[2]))
+        
 
-
-
-    return addition, subtraction
+    return problem_dict
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-print('hello')
