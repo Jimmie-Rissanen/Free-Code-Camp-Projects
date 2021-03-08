246eb4d8c2a28e07033ea3dc0801e5646cc6da56
Jimmie <jimmieriss@gmail.com>
2021-03-08 19:39:37 +0100

    Http

---
+++ python/Arithmetic_arrenger.py
@@ -0,0 +1,19 @@
+def arithmetic_arranger(problems, optional=False):
+    
+    # Handle input
+
+    # Compute 
+
+    for problem in problems: 
+        ps = problem.split()
+        if ps[1] == "+":
+            addition = int(ps[0]) + int(ps[2])
+        if ps[1] == "-":
+            addition = int(ps[0]) - int(ps[2])
+
+    # Arrange problem
+
+    return addition, subtraction
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print('hello')
