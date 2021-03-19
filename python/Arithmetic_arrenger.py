aeba72dd3b383f009a56443d7de7109c4f2b8f72
Jimmie <jimmieriss@gmail.com>
2021-03-19 22:09:51 +0100

    working with str.format

--- python/Arithmetic_arrenger.py
+++ python/Arithmetic_arrenger.py
@@ -1,7 +1,11 @@
 def arithmetic_arranger(problems, optional=True):
     # Lists that contian the answeres.
+    num_row = ""
+    op_row = ""
+    dash_row = ""
+    answere_row = ""
+    sep = "    "
     answers = []
-
     if len(problems) > 5:
         return 'Error: Too many problems.'
     # Extract the relevant parts of the list.
@@ -13,7 +17,7 @@ def arithmetic_arranger(problems, optional=True):
 
         # Calculate to be able to show answere
 
-        if not first.isdigit() or not second.isdigit():
+        if not first.isnumeric() or not second.isnumeric():
             return 'Error: Numbers must only contain digits.'
         # Get the length and align the problem.
         if op == '+':
@@ -23,21 +27,18 @@ def arithmetic_arranger(problems, optional=True):
         elif not op == '+' or not op == '-': 
             return "Error: Operator must be '+' or '-'."
         solution = str(solution)
-        length = len(max(problem))
+        length = max(len(first), len(second)) 
+        dashes = '-' * (length + 2)
         if length > 4:
             return 'Error: Numbers cannot be more than four digits.'
-        dashes = '-' * length
-            
-        #Return different lists depending on true or false.   
-        if optional is True:
-            stacked = f"{first.rjust(6)}\n{op}{second.rjust(5)}\n{dashes.rjust(5)}\n{solution.rjust(5)}"
-            stacked.strip()
-            answers.append(stacked)
-        else:
-            stacked = f"{first.rjust(6)}\n{op}{second.rjust(5)}\n{dashes.rjust(5)}"
-            stacked.strip()
-            answers.append(stacked)
-    print("    ".join(answers))
+        
+        num_row = '{:*>{width}}'.format(first, width=length)
+        op_row = '{:*>{width}}'.format(op, width=length)
+        dash_row = '{:*>{width}}'.format(second, width=length)          #testade output med hjälp av str.format()
+        answere_row = '{:*>{width}}'.format(dashes, width=length)       #hur kan jag göra så att alla värden 
+        print(num_row,op_row,dash_row,)                                 #kommer in utan hard coding? 
+        print(answere_row)                                              # for loop och dictionary? 
+    
     
          
 arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 443", "123 + 449"])
